09205885f7d0c1320168184059e2e4601ebd737c
Felipe Acosta <facosta0787@gmail.com>
2021-04-29 00:10:47 -0500

    VDU-5 | Small refactor and added documentation

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Vercel DNS Records Update/Create
 
-### Install
+## Install
+
+### Command
 
 ```bash
 curl -o- https://raw.githubusercontent.com/facosta0787/vercel-dns-update/main/tools/install.sh | sh
 ```
 
-#### Author
+### Options
+
+| Option | Description |
+| ------ | ----------- |
+| `--domain` | Domain to update the records|
+| `--records` | Recrods to update, It can be comma separeted list|
+
+#### Example
+
+```bash
+TOKEN=<your-vercel-token> node bin/dns-update --domain=yourdomain.com --records=record1,record2
+```
+### Author
+
 @facosta0787
--- index.js
+++ index.js
@@ -1,68 +1,30 @@
 require('dotenv').config()
-const format  = require('date-fns/fp/format').default
-const request = require('./lib/request')(process.env.TOKEN);
-const argv    = require('process.argv')(process.argv.slice(2));
+const format       = require('date-fns/fp/format').default
+const request      = require('./lib/request')(process.env.TOKEN)
+const argv         = require('process.argv')(process.argv.slice(2))
+const updateRecord = require('./lib/update-record').default
 
-const config = argv({ update: true, domain: null, records: '' });
+const config = argv({ update: true, domain: null, records: '' })
 
 async function app() {
-  const { domain } = config;
-  const records = config.records.includes(',') ? config.records.split(',') : [config.records];
+  const { domain } = config
+  const records = config.records.includes(',') ? config.records.split(',') : [config.records]
 
   if (!domain || records.length < 0) {
-    console.error('Error: You must to supply a domain and at least one record');
-    process.exit(1);
+    console.error('Error: You must to supply a domain and at least one record')
+    process.exit(1)
   }
 
   try {
-    const { data } = await request('https://api.myip.com');
-    const publicIp = data.ip;
-    const responses = await Promise.all(records.map(record => updateRecord(domain, record, publicIp)));
-    console.log(format('yyyy-MM-dd HH:mm:ss')(new Date()) + '\n  ' + responses.join('\n  '));
-    process.exit(0);
+    const { data } = await request('https://api.myip.com')
+    const publicIp = data.ip
+    const responses = await Promise.all(records.map(record => updateRecord(domain, record, publicIp)))
+    console.log(format('yyyy-MM-dd HH:mm:ss')(new Date()) + '\n  ' + responses.join('\n  '))
+    process.exit(0)
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   }
 }
 
-async function updateRecord(domainName, recordName, ip) {
-  const baseURL = 'https://api.vercel.com';
-  const dnsListPath = baseURL + '/v4/domains/:domain/records'.replace(/:domain/gi, domainName);
-  const dnsRemovePath = baseURL + '/v2/domains/:domain/records/:recId'.replace(/:domain/gi, domainName);
-  const dnsCreatePath = baseURL + '/v2/domains/:domain/records'.replace(/:domain/gi, domainName);
-  const { data } = await request(dnsListPath);
-  const record = data.records.find(dnsRecord => dnsRecord.name === recordName);
-
-  if (record && record.value === ip) {
-    return `Info: Record ${recordName}@${domainName} is already up to date with ip ${ip}`;
-  }
-
-  if (record) {
-    await request(dnsRemovePath.replace(/:recId/gi, record.id), {
-      method: 'DELETE'
-    });
-  }
-
-  const body = {
-    name: recordName,
-    type: 'A',
-    value: ip,
-    ttl: 60
-  };
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(body)
-  };
-  const {
-    data: {
-      uid
-    }
-  } = await request(dnsCreatePath, options);
-
-  if (uid) {
-    return `Info: Record ${recordName}@${domainName} ${record ? 'updated' : 'created'} successfully with ip address ${ip}`;
-  }
-}
-
-app();
+app()
